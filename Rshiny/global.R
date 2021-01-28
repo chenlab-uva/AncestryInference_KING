208822b7cd32f07ba82dad8ee14d627e1fe4e51b
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-01-28 16:04:31 -0500

    Update global.R

--- Rshiny/global.R
+++ Rshiny/global.R
@@ -3,6 +3,10 @@ library(ggplot2)
 options(scipen = 999)
 options(shiny.maxRequestSize = 30 * 1024^2)
 
-Palette <- c("#1F78B4", "#33A02C", "#E31A1C", "#FF7F00", "#6A3D9A", "#B15928", "#A6CEE3", 
+predefined.col <- c("#1F78B4", "#33A02C", "#E31A1C", "#FF7F00", "#6A3D9A", "#B15928", "#A6CEE3", 
              "#B2DF8A", "#FB9A99", "#FDBF6F", "#CAB2D6", "#FFFF99", "#999999")
+ggcolor_hue <- function(n) {
+  hues = seq(15, 375, length = n + 1)
+  hcl(h = hues, l = 65, c = 100)[1:n]
+}
 
