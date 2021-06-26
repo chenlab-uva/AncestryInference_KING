3cbf4513b371fd29cb55bb5397ce06e01a98b127
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-06-25 20:17:07 -0400

    Update ui.R

--- Rshiny/ui.R
+++ Rshiny/ui.R
@@ -1,35 +1,27 @@
-# ui.R
 ui <- fluidPage( 
   titlePanel(("Interface for Interactive Plot of Ancestry")),
-  sidebarLayout(position = "left",
+  sidebarLayout(position = "left", 
                 sidebarPanel( 
-                  fileInput("file1", "Choose a text file with ancestry information",accept = "text"),
-                  selectInput("Ancestry", label = "Ancestry", choices = c(Choose=''), selected = NULL),
-                  #textInput("FID_IID", "FID_IID", value = ""),
-                  fluidRow(
-                    column(5, textInput(inputId = "SampleFID",
-                                        label = "FID",
-                                        value = " ",
-                                        width = "100px")
-                    ),
-                    column(5, ofset = 3,
-                           textInput(inputId = "SampleIID",
-                                     label = "IID",
-                                     value = "",
-                                     width = "100px")
-                    )),
-                  actionButton(inputId = "EnterID", label = "Enter Sample FID and IID"),
-                  width = 3
+                  fileInput("file1", "Step1: Please choose a *.txt file with ancestry information",accept = "text"),
+                  selectInput("Ancestry", label = "Step 2: Choose the ancestry group", choices = c(Choose=''), selected = NULL),
+                  textInput(inputId = "FID", label = "Or Step 2b: please specify the family size", value = " "),
+                  actionButton(inputId = "EnterFID", label = "Submit Family ID"),
+                  width = 2
                 ),
                 mainPanel(
                   fluidRow(
-                    plotOutput(outputId = "plot1", width = "100%"),
-                    plotOutput(outputId = "plot2", width = "100%", click = "plot_click")),
+                    splitLayout(
+                      plotOutput(outputId = "plot1", width = "100%"),
+                      plotOutput(outputId = "plot2", width = "100%", click = "plot_click"))
+                  ),
+                  fluidRow(
+                    dataTableOutput('table1'),
+                    dataTableOutput('table2')
+                  ),
                   fluidRow(
                     column(width = 12,
                            verbatimTextOutput("click_info"),
-                           verbatimTextOutput("last_infor"),
-                           dataTableOutput('table'))))
+                           verbatimTextOutput("last_infor"))))
                 
   )
 )
