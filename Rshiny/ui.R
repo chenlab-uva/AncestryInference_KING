37059595223d70f88d22b59548dc19cdf9394c2f
Zhennan Zhu <48361064+Zhennan-Z@users.noreply.github.com>
2021-07-08 16:13:41 -0400

    Update ui.R

--- Rshiny/ui.R
+++ Rshiny/ui.R
@@ -4,7 +4,7 @@ ui <- fluidPage(
                 sidebarPanel( 
                   fileInput("file1", "Step1: Please choose a *.txt file with ancestry information",accept = "text"),
                   selectInput("Ancestry", label = "Step 2: Choose the ancestry group", choices = c(Choose=''), selected = NULL),
-                  textInput(inputId = "FID", label = "Or Step 2b: please specify the family size", value = " "),
+                  textInput(inputId = "FID", label = "Or Step 2b: Please type a family ID", value = " "),
                   actionButton(inputId = "EnterFID", label = "Submit Family ID"),
                   width = 2
                 ),
